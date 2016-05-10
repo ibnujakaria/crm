8d168f4b127df1bc753e5b8c092d8dd0c1ebd0ac
fareyhanz <uchiha.reyhan@gmail.com>
2016-05-10 14:50:08 +0700

    menghapus campaign dari dashbor

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -28,19 +28,6 @@
     <!-- small box -->
     <div class="small-box bg-green">
       <div class="inner">
-        <h3>53</h3>
-        <p>Campaigns</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-stats-bars"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-    </div>
-  </div><!-- ./col -->
-  <div class="col-lg-3 col-xs-6">
-    <!-- small box -->
-    <div class="small-box bg-yellow">
-      <div class="inner">
         <h3>44</h3>
         <p>Sales</p>
       </div>
